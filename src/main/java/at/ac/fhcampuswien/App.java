c9f6a00c7b375e00ec6d89692234e5f30ac185fe
Benjamin Lichtenstein <31620900+bennilic@users.noreply.github.com>
2022-10-12 18:26:58 +0200

    Task 1

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -7,10 +7,46 @@ public class App {
 
     // Implement all methods as public static
 
+    public static void oneMonthCalendar(int daysInMonth, int firstWeekdayOfMonth) {
+        for (int i = 1; i < firstWeekdayOfMonth; i++) {
+            System.out.print("   ");
+        }
+
+        int countUntilLineBreak = 0;
+        int firstRowComplete = 8 - firstWeekdayOfMonth;
+        for (int i = 1; i <= daysInMonth; i++) {
+            if (i < 10) {
+                System.out.print(" " + i + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+
+            if (i == firstRowComplete) {
+                System.out.println();
+                countUntilLineBreak = 0;
+                continue;
+            } else {
+                countUntilLineBreak++;
+            }
+
+            if (countUntilLineBreak % 7 == 0) {
+                System.out.println();
+            }else {
+                if (i == daysInMonth){
+                    System.out.println();
+                }
+            }
+        }
+
+//        if (firstWeekdayOfMonth != 1) System.out.println();
+    }
+
     public static void main(String[] args) {
         // test your method implementations here
         // make method calls
         // print their results
         // etc.
+
+        oneMonthCalendar(30, 6);
     }
 }
