66494b7a93926639688ee0ca55881031a022acc3
Benjamin Lichtenstein <31620900+bennilic@users.noreply.github.com>
2022-10-15 09:53:31 +0200

    Task 5

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -33,21 +33,21 @@ public class App {
 
             if (countUntilLineBreak % 7 == 0) {
                 System.out.println();
-            }else {
-                if (i == daysInMonth){
+            } else {
+                if (i == daysInMonth) {
                     System.out.println();
                 }
             }
         }
     }
 
-    public static long[] lcg(long seed){
+    public static long[] lcg(long seed) {
         long[] lcg = new long[10];
         final long M = (long) Math.pow(2, 31);
         final long A = 1103515245;
         final long C = 12345;
 
-        for (int i = 0; i < lcg.length; i++){
+        for (int i = 0; i < lcg.length; i++) {
             lcg[i] = (A * seed + C) % M;
             seed = lcg[i];
         }
@@ -55,20 +55,20 @@ public class App {
         return lcg;
     }
 
-    public static void guessingGame(int numberToGuess){
+    public static void guessingGame(int numberToGuess) {
         Scanner scanner = new Scanner(System.in);
 
         int n;
-        for (int i = 0; i < 10; i++){
-            System.out.printf("Guess number %d: ", i+1);
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Guess number %d: ", i + 1);
             n = scanner.nextInt();
 
-            if (i == 9){
+            if (i == 9) {
                 System.out.println("You lost! Have you ever heard of divide & conquer?");
                 return;
             }
 
-            if (n == numberToGuess){
+            if (n == numberToGuess) {
                 System.out.println("You won wisenheimer!");
                 return;
             }
@@ -78,21 +78,49 @@ public class App {
         }
     }
 
-    public static int randomNumberBetweenOneAndHundred(){
+    public static int randomNumberBetweenOneAndHundred() {
         Random random = new Random();
         return random.nextInt(1, 101);
     }
 
-    public static boolean swapArrays(int[] a, int[] b){
+    public static boolean swapArrays(int[] a, int[] b) {
         if (a.length != b.length) return false;
 
-        for (int i = 0; i < a.length; i++){
+        for (int i = 0; i < a.length; i++) {
             a[i] = a[i] + b[i];
             b[i] = a[i] - b[i];
             a[i] = a[i] - b[i];
         }
 
         return true;
+    }
+
+    public static String camelCase(String s) {
+        String camelCaseSentence = "";
+        char[] chars = s.toCharArray();
+
+        char c;
+        for (int i = 0; i < chars.length; i++){
+            c = chars[i];
+            boolean isUpperCaseLetter = c > 64 && c < 91;
+            boolean isLowerCaseLetter = c > 96 && c < 123;
+
+            if (i == 0){
+                if(isUpperCaseLetter) camelCaseSentence += c;
+                if(isLowerCaseLetter) camelCaseSentence += (char) (c-32);
+            }else{
+                boolean isLastCharSpace = chars[i-1] == 32;
+                if (isLastCharSpace){
+                    if(isUpperCaseLetter) camelCaseSentence += c;
+                    if(isLowerCaseLetter) camelCaseSentence += (char) (c-32);
+                }else{
+                    if (isUpperCaseLetter) camelCaseSentence += (char) (c+32);
+                    if (isLowerCaseLetter) camelCaseSentence += c;
+                }
+            }
+        }
+
+        return camelCaseSentence;
     }
 
     public static void main(String[] args) {
